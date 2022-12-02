4660ec643d769d64fcf39536caa227914f4ee7ce
Stepan Vovk <stepan.vovk@plvision.eu>
2022-12-02 04:29:10 +0200

    hw#2, printed tests
    Signed-off-by: Stepan Vovk <stepan.vovk@plvision.eu>

--- stub/src/stub_sai_lag.c
+++ stub/src/stub_sai_lag.c
@@ -1,22 +1,95 @@
-/*
- *  Copyright (C) 2014. Mellanox Technologies, Ltd. ALL RIGHTS RESERVED.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License"); you may
- *    not use this file except in compliance with the License. You may obtain
- *    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- *    THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
- *    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
- *    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
- *    FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
- *
- *    See the Apache Version 2.0 License for specific language governing
- *    permissions and limitations under the License.
- *
- */
-
 #include "sai.h"
 #include "stub_sai.h"
+#include "assert.h"
 
-#undef  __MODULE__
-#define __MODULE__ SAI_LAG
+sai_status_t stub_create_lag(
+    _Out_ sai_object_id_t* lag_id,
+    _In_ uint32_t attr_count,
+    _In_ sai_attribute_t *attr_list)
+{
+    static int32_t next_lag_id = 1;
+    sai_status_t status;
+    status = stub_create_object(SAI_OBJECT_TYPE_LAG, next_lag_id++, lag_id);
+    if (status != SAI_STATUS_SUCCESS) {
+        printf("Cannot create a LAG OID\n");
+        return status;
+    }
+    printf("CREATE LAG: 0x%lX\n", *lag_id);
+    return status;
+}
+
+sai_status_t stub_remove_lag(
+    _In_ sai_object_id_t  lag_id)
+{
+    printf("REMOVE LAG:%lX\n", lag_id);
+    return SAI_STATUS_SUCCESS;
+}
+
+sai_status_t stub_set_lag_attribute(
+    _In_ sai_object_id_t  lag_id,
+    _In_ const sai_attribute_t *attr)
+{
+    printf("SET_LAG#%lX\n ", lag_id);
+    return SAI_STATUS_SUCCESS;
+}
+
+sai_status_t stub_get_lag_attribute(
+    _In_ sai_object_id_t lag_id,
+    _In_ uint32_t attr_count,
+    _Inout_ sai_attribute_t *attr_list)
+{
+    printf("GET_LAG#%lX\n", lag_id);
+    return SAI_STATUS_SUCCESS;
+}
+
+sai_status_t stub_create_lag_member(
+    _Out_ sai_object_id_t* lag_member_id,
+    _In_ uint32_t attr_count,
+    _In_ sai_attribute_t *attr_list)
+{
+    static int32_t next_lag_member_id = 1;
+    sai_status_t status;
+
+    status = stub_create_object(SAI_OBJECT_TYPE_LAG_MEMBER , next_lag_member_id++, lag_member_id);
+    if (status != SAI_STATUS_SUCCESS) {
+        printf("Cannot create a LAG MEMBER OID\n");
+        return status;
+    }
+    printf("CREATE LAG_MEMBER: 0x%lX {LAG_ID: 0x%lX, PORT_ID: 0x%lX}\n", *lag_member_id, attr_list[0].value.oid, attr_list[1].value.oid);
+    return status;
+}
+
+sai_status_t stub_remove_lag_member(
+    _In_ sai_object_id_t  lag_member_id)
+{
+    printf("REMOVE LAG_MEMBER:%lX\n", lag_member_id);
+    return SAI_STATUS_SUCCESS;
+}
+
+sai_status_t stub_set_lag_member_attribute(
+    _In_ sai_object_id_t  lag_member_id,
+    _In_ const sai_attribute_t *attr)
+{
+    printf("SET_LAG_MEMBER:%lX\n", lag_member_id);
+    return SAI_STATUS_SUCCESS;
+}
+
+sai_status_t stub_get_lag_member_attribute(
+    _In_ sai_object_id_t lag_member_id,
+    _In_ uint32_t attr_count,
+    _Inout_ sai_attribute_t *attr_list)
+{
+    printf("GET_LAG_MEMBER:%lX\n", lag_member_id);
+    return SAI_STATUS_SUCCESS;
+}
+
+const sai_lag_api_t lag_api = {
+    stub_create_lag,
+    stub_remove_lag,
+    stub_set_lag_attribute,
+    stub_get_lag_attribute,
+    stub_create_lag_member,
+    stub_remove_lag_member,
+    stub_set_lag_member_attribute,
+    stub_get_lag_member_attribute
+};
